b1053c1a3685d1a5c39825bcb980775c95af84ea
Ivan Shanygin <shanygin101@yandex.ru>
2024-12-01 17:05:21 +0300

    [HW-3]: Add stress strategy to MSQueueStraightforwardTest

--- hw_3/src/test/kotlin/MSQueueStraightforwardTest.kt
+++ hw_3/src/test/kotlin/MSQueueStraightforwardTest.kt
@@ -1,6 +1,7 @@
 import org.jetbrains.kotlinx.lincheck.*
 import org.jetbrains.kotlinx.lincheck.annotations.*
 import org.jetbrains.kotlinx.lincheck.strategy.managed.modelchecking.ModelCheckingOptions
+import org.jetbrains.kotlinx.lincheck.strategy.stress.StressOptions
 import java.util.*
 import java.util.Optional.empty
 import java.util.Optional.of
@@ -16,7 +17,14 @@ class MSQueueStraightforwardTest {
     fun dequeue(): Optional<Int> = q.dequeue()
 
     @Test
-    fun stressSequentialSpecTest() = ModelCheckingOptions()
+    fun modelCheckSequentialSpecTest() = ModelCheckingOptions()
+        .actorsBefore(10)
+        .threads(3).actorsPerThread(3).iterations(3)
+        .actorsAfter(10)
+        .sequentialSpecification(SequentialQueue::class.java)
+        .check(this::class)
+    @Test
+    fun stressSequentialSpecTest() = StressOptions()
         .actorsBefore(10)
         .threads(3).actorsPerThread(3).iterations(3)
         .actorsAfter(10)
